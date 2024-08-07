b64ff665f17cea437cccd478087b5937c78152aa
ArmanLK <cheshmiarman@gmail.com>
2024-08-07 17:29:28 +0330

    changed vulkman.cpp to triangle.cpp

--- README.md
+++ README.md
@@ -2,4 +2,9 @@ First I'm trying out [vulkan-tutorial](https://vulkan-tutorial.com/).
 The probably creating my own rendering engine.
 
 I'm using zig build system instead of Cmake.
+
+# The Triangle Demo
+```bash
+zig build triangle
+```
 ![The Triangle](https://github.com/ArmanLK/vulkman/blob/master/triangle.png)
--- build.zig
+++ build.zig
@@ -4,22 +4,22 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "vulkman",
+    const triangle = b.addExecutable(.{
+        .name = "triangle",
         //.root_source_file = b.path("src/vulkman.cpp"),
         .target = target,
         .optimize = optimize,
-        .use_lld = true,
-        .pic = true,
+        //.use_lld = true,
+        //.pic = true,
         .linkage = .dynamic,
     });
-    exe.linkLibCpp();
-    exe.linkSystemLibrary("vulkan");
-    exe.linkSystemLibrary("glfw");
-    exe.addCSourceFiles(.{
+    triangle.linkLibCpp();
+    triangle.linkSystemLibrary("vulkan");
+    triangle.linkSystemLibrary("glfw");
+    triangle.addCSourceFiles(.{
         .root = .{ .cwd_relative = "" },
         .files = &.{
-            "src/vulkman.cpp",
+            "src/triangle.cpp",
         },
         .flags = &.{
             "-std=c++17",
@@ -29,9 +29,9 @@ pub fn build(b: *std.Build) void {
         },
     });
 
-    b.installArtifact(exe);
+    b.installArtifact(triangle);
 
-    const run_cmd = b.addRunArtifact(exe);
+    const run_cmd = b.addRunArtifact(triangle);
 
     run_cmd.step.dependOn(b.getInstallStep());
 
@@ -39,6 +39,6 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("triangle", "Run the triangle demo");
     run_step.dependOn(&run_cmd.step);
 }
