b47f16c665717a3fcd6643d23b09704a463306d1
ArmanLK <cheshmiarman@gmail.com>
2024-08-30 23:41:40 +0330

    started the zig version (not working)

--- build.zig
+++ build.zig
@@ -4,12 +4,97 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const cglm = b.addStaticLibrary(.{
+        .name = "cglm",
+        .target = target,
+        .optimize = b.standardOptimizeOption(.{
+            .preferred_optimize_mode = .ReleaseSafe,
+        }),
+    });
+    cglm.linkLibC();
+    cglm.addIncludePath(.{
+        .cwd_relative = "lib/cglm/include/",
+    });
+    cglm.addCSourceFiles(.{
+        .root = .{
+            .cwd_relative = "lib/cglm/",
+        },
+        .files = &.{
+            "src/euler.c",
+            "src/affine.c",
+            "src/io.c",
+            "src/quat.c",
+            "src/cam.c",
+            "src/vec2.c",
+            "src/ivec2.c",
+            "src/vec3.c",
+            "src/ivec3.c",
+            "src/vec4.c",
+            "src/ivec4.c",
+            "src/mat2.c",
+            "src/mat2x3.c",
+            "src/mat2x4.c",
+            "src/mat3.c",
+            "src/mat3x2.c",
+            "src/mat3x4.c",
+            "src/mat4.c",
+            "src/mat4x2.c",
+            "src/mat4x3.c",
+            "src/plane.c",
+            "src/frustum.c",
+            "src/box.c",
+            "src/project.c",
+            "src/sphere.c",
+            "src/ease.c",
+            "src/curve.c",
+            "src/bezier.c",
+            "src/ray.c",
+            "src/affine2d.c",
+            "src/clipspace/ortho_lh_no.c",
+            "src/clipspace/ortho_lh_zo.c",
+            "src/clipspace/ortho_rh_no.c",
+            "src/clipspace/ortho_rh_zo.c",
+            "src/clipspace/persp_lh_no.c",
+            "src/clipspace/persp_lh_zo.c",
+            "src/clipspace/persp_rh_no.c",
+            "src/clipspace/persp_rh_zo.c",
+            "src/clipspace/view_lh_no.c",
+            "src/clipspace/view_lh_zo.c",
+            "src/clipspace/view_rh_no.c",
+            "src/clipspace/view_rh_zo.c",
+            "src/clipspace/project_no.c",
+            "src/clipspace/project_zo.c",
+        },
+        .flags = &.{
+            "-Wall",
+            "-Werror",
+            "-std=c11",
+        },
+    });
+    b.installArtifact(cglm);
+
+    const vulkman = b.addExecutable(.{
+        .name = "vulkman",
+        .target = target,
+        .optimize = optimize,
+        .linkage = .dynamic,
+        .root_source_file = .{
+            .cwd_relative = "src/main.zig",
+        },
+    });
+    vulkman.linkSystemLibrary("vulkan");
+    vulkman.linkSystemLibrary("glfw");
+
+    b.installArtifact(vulkman);
+
     const triangle = b.addExecutable(.{
         .name = "triangle",
         .target = target,
         .optimize = optimize,
         .linkage = .dynamic,
     });
+
+    const vulkman_run_cmd = b.addRunArtifact(vulkman);
 
     triangle.linkLibCpp();
     triangle.linkSystemLibrary("vulkan");
@@ -110,15 +195,20 @@ pub fn build(b: *std.Build) void {
     const image_run_cmd = b.addRunArtifact(image);
 
     if (b.args) |args| {
+        vulkman_run_cmd.addArgs(args);
         triangle_run_cmd.addArgs(args);
         rectangle_run_cmd.addArgs(args);
         rectangle_3d_run_cmd.addArgs(args);
         image_run_cmd.addArgs(args);
     }
 
+    vulkman_run_cmd.step.dependOn(b.getInstallStep());
     triangle_run_cmd.step.dependOn(b.getInstallStep());
     rectangle_run_cmd.step.dependOn(b.getInstallStep());
     rectangle_3d_run_cmd.step.dependOn(b.getInstallStep());
+
+    const vulkman_run_step = b.step("run", "Run the main app");
+    vulkman_run_step.dependOn(&vulkman_run_cmd.step);
 
     const triangle_run_step = b.step("triangle", "Run the triangle demo");
     triangle_run_step.dependOn(&triangle_run_cmd.step);
